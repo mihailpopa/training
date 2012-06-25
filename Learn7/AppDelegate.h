4cce23a2496f55444e57e5c2d5eb18bebe587bdb
Mihai Popa <mihai@Xoomworkss-iMac.local>
2012-06-25 09:20:52 +0300

    Initial Commit

---
+++ Learn7/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Learn7
+//
+//  Created by Mihai Popa on 6/25/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
